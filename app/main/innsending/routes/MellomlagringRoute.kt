00676ac516d9746f4c8a28c8c64d9efb93576184
VHollund <vetlehoo@gmail.com>
2023-12-04 14:51:13 +0100

    mellomlagring tar nå i mot MultipartForm for å gjøre det letter for frontend filopplastning
    Co-authored-by: Halvor Grizzly Bjørn <halvor.grizzly.bjorn@nav.no>
    Co-authored-by: Richard Borge <richard.borge@nav.no>
    Co-authored-by: Sturle Helland <sturle.helland@nav.no>
    Co-authored-by: Robin Tordly <robin.tordly@nav.no>

--- app/main/innsending/routes/MellomlagringRoute.kt
+++ app/main/innsending/routes/MellomlagringRoute.kt
@@ -4,6 +4,7 @@ import innsending.antivirus.ClamAVClient
 import innsending.pdf.PdfGen
 import innsending.redis.Redis
 import io.ktor.http.*
+import io.ktor.http.content.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
 import io.ktor.server.auth.jwt.*
@@ -50,31 +51,43 @@ fun Route.mellomlagerRoute(redis: Redis, virusScanClient: ClamAVClient, pdfGen: 
     route("/mellomlagring/fil") {
         post {
             val filId = UUID.randomUUID().toString()
-            val fil = call.receive<ByteArray>()
-            val contentType = ContentType.parse(requireNotNull(call.request.header(HttpHeaders.ContentType)))
-            val acceptedContentType = listOf(ContentType.Image.JPEG, ContentType.Image.PNG, ContentType.Application.Pdf)
 
-            val pdf: ByteArray = when (contentType){
-                 in acceptedContentType -> {
-                    if (virusScanClient.hasVirus(fil, contentType)) {
-                        return@post call.respond(HttpStatusCode.NotAcceptable, "Fant virus i fil")
+            when (val mottattFil = call.receiveMultipart().readAllParts().single()) {
+                is PartData.FileItem -> {
+                    val fil = mottattFil.streamProvider().readBytes()
+                    val contentType = requireNotNull(mottattFil.contentType) { "contentType i multipartForm mangler" }
+                    val acceptedContentType =
+                        listOf(ContentType.Image.JPEG, ContentType.Image.PNG, ContentType.Application.Pdf)
+
+                    val pdf: ByteArray = when (contentType) {
+                        in acceptedContentType -> {
+                            if (virusScanClient.hasVirus(fil, contentType)) {
+                                return@post call.respond(HttpStatusCode.NotAcceptable, "Fant virus i fil")
+                            }
+
+                            pdfGen.bildeTilPfd(fil, contentType)
+                        }
+
+                        else -> {
+                            return@post call.respond(HttpStatusCode.NotAcceptable, "Filtype ikke støttet")
+                        }
                     }
 
-                    pdfGen.bildeTilPfd(fil, contentType)
+                    if (!sjekkPdf(pdf)) {
+                        return@post call.respond(HttpStatusCode.NotAcceptable, "PDF er kryptert")
+                    }
+
+                    redis[filId] = pdf
+                    redis.expire(filId, 3 * EnDag)
+
+                    call.respond(HttpStatusCode.Created, filId)
                 }
+
                 else -> {
                     return@post call.respond(HttpStatusCode.NotAcceptable, "Filtype ikke støttet")
                 }
+
             }
-
-            if (!sjekkPdf(pdf)) {
-                return@post call.respond(HttpStatusCode.NotAcceptable, "PDF er kryptert")
-            }
-
-            redis[filId] = pdf
-            redis.expire(filId, 3 * EnDag)
-
-            call.respond(HttpStatusCode.Created, filId)
         }
 
         get("/{filId}") {
