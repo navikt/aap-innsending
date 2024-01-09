599d522898384299a7d751a8f762f98e9a5f5d80
Richard Borge <richard.borge@nav.no>
2024-01-09 09:46:00 +0100

    Legger på sti for å hente søknader så det blir klarere.

--- app/main/innsending/routes/InnsendnigRoute.kt
+++ app/main/innsending/routes/InnsendnigRoute.kt
@@ -18,7 +18,7 @@ private val logger = LoggerFactory.getLogger("App")
 fun Route.innsendingRoute(postgres: PostgresRepo, redis: Redis) {
     route("/innsending") {
 
-        get {
+        get("/soknader") {
             val personIdent = call.personident()
 
             call.respond(postgres.hentAlleSøknader(personIdent))
