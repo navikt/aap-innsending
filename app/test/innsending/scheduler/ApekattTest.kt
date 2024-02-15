6bc4e4f940aee92be2996f9cecf37d4d4b63c00c
Halvor Bjørn <halvor.grizzly.bjorn@nav.no>
2024-02-15 12:44:59 +0100

    Fikser test

--- app/test/innsending/scheduler/ApekattTest.kt
+++ app/test/innsending/scheduler/ApekattTest.kt
@@ -118,7 +118,7 @@ class ApekattTest : H2TestBase() {
         bruker = Journalpost.Bruker(id = Journalpost.Fødselsnummer("12345678910")),
         dokumenter = listOf(
             Journalpost.Dokument(
-                tittel = "Søknad",
+                tittel = "Søknad om arbeidsavklaringspenger",
                 brevkode = "NAV 11-13.05",
                 dokumentVarianter = listOf(
                     Journalpost.DokumentVariant(
