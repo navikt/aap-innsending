38e1dabb800851710bec424bf1406ae30c7952db
Fredrik Meyer <fredrik.meyer@nav.no>
2025-09-05 08:26:42 +0200

    Ikke exception ved timeout på ready-endepunktet

--- app/main/innsending/routes/ActuatorRoute.kt
+++ app/main/innsending/routes/ActuatorRoute.kt
@@ -6,6 +6,8 @@ import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.micrometer.prometheusmetrics.PrometheusMeterRegistry
 
+private val log = org.slf4j.LoggerFactory.getLogger("ActuatorRoute")
+
 fun Routing.actuator(prometheus: PrometheusMeterRegistry, redisRepo: Redis) {
     route("/actuator") {
         get("/metrics") {
@@ -15,7 +17,13 @@ fun Routing.actuator(prometheus: PrometheusMeterRegistry, redisRepo: Redis) {
             call.respond(HttpStatusCode.OK, "live")
         }
         get("/ready") {
-            if(redisRepo.ready()){
+            val redisReady = try {
+                redisRepo.ready()
+            } catch (e: Exception) {
+                log.info("Redis ready feilet.", e)
+                false
+            }
+            if (redisReady) {
                 call.respond(HttpStatusCode.OK, "ready")
             }
             call.respond(HttpStatusCode(503, "Service Unavailable"))
