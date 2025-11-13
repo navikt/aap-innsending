3f4c9b5505a56af08cf53526fe1ca718b75aa0f5
Per Henden <per.christian.henden@nav.no>
2025-11-13 10:46:21 +0100

    Bump testcontainters version
    to remain compatible with current docker version in brew on arm64, e.g. M4 macbooks

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,12 +66,8 @@ dependencies {
             because("https://github.com/advisories/GHSA-4g9r-vxhx-9pgx")
         }
     }
-    testImplementation("org.testcontainers:postgresql:1.21.3")
-    constraints {
-        implementation("org.apache.commons:commons-compress:1.28.0") {
-            because("https://github.com/advisories/GHSA-4g9r-vxhx-9pgx")
-        }
-    }
+    testImplementation("org.testcontainers:testcontainers-postgresql:2.0.1")
+
     testImplementation("org.assertj:assertj-core:3.27.6")
 
 }
